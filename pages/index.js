78d5f3d005b1f1fdd5242bba3195f6a1d70e2710
José Augusto Azevedo Genovez de Mesquita Braga <joseaugusto.braga@gmail.com>
2024-09-09 14:12:33 -0300

    Alteração do texto da página

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 function Home(){
-    return <h1>Acredito que seja a terceira vez que tenho que alterar o texto por error besta no português.
-        Como o projeto é meu e já estou totó das ideias, estou usando esse espaço para desabafar e falar algo muito importante:
-        ÁLVARO, VOCÊ É BOBO E A LOREN É LINDA!
-        Obs: eu não sei como fazer quebra de linha, então para você está tudo em sequência...</h1>
+    return <h1>Página dedicada aos estudos do ZeBraga com o curso Curso.dev!</h1>
 }
 
 export default Home;
