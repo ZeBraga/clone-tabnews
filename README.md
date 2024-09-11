0fe77ebde9d8f3761c875a950bd272d41fe8cb83
José Augusto Azevedo Genovez de Mesquita Braga <joseaugusto.braga@gmail.com>
2024-09-11 08:58:36 -0300

    Adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implentação do www.tabnews.com.br para o curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Página dedicada aos estudos do ZeBraga com o curso Curso.dev!</h1>
+function Home() {
+  return <h1>Página dedicada aos estudos do ZeBraga com o curso Curso.dev!</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function test2() {
+  console.log("Teste2");
 }
 
 export default Home;
